a315c18367efaead5722d9684cff6b6da240f51c
Juan Pina <juanpinaribeiro@outlook.com>
2024-10-28 13:29:54 -0300

    Classe "Candidado": entidade e controller

---
+++ src/main/java/br/com/juanribeiro/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,18 @@
+package br.com.juanribeiro.gestao_vagas.modules.candidate;
+
+import java.util.UUID;
+
+import lombok.Data;
+
+@Data
+public class CandidateEntity {
+
+  private UUID id;
+  private String name;
+  private String username;
+  private String email;
+  private String password;
+  private String despription;
+  private String curriculum;
+  
+}
