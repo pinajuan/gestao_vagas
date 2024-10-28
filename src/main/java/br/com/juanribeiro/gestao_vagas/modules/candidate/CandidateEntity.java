a813aadf20d0648894fe6b720f47deb99f7a53fb
Juan Pina <juanpinaribeiro@outlook.com>
2024-10-28 17:36:08 -0300

    Criando tabela "candidate" no banco de dados

--- src/main/java/br/com/juanribeiro/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/juanribeiro/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,17 +1,26 @@
 package br.com.juanribeiro.gestao_vagas.modules.candidate;
 
 import java.util.UUID;
+import java.time.LocalDateTime;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
   private String name;
 
@@ -26,5 +35,8 @@ public class CandidateEntity {
   private String password;
   private String despription;
   private String curriculum;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
   
 }
