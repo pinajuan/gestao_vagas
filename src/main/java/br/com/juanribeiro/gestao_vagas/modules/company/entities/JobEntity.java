5036673948653412286e2ceab26e0fe4cf426ed3
Juan Pina <juanpinaribeiro@outlook.com>
2024-10-30 10:07:43 -0300

    UseCase e Controller de "Job" e alguns ajustes

--- src/main/java/br/com/juanribeiro/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/juanribeiro/gestao_vagas/modules/company/entities/JobEntity.java
@@ -12,6 +12,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
@@ -26,13 +27,14 @@ public class JobEntity {
 
   private String benefits;
 
+  @NotBlank(message = "O campo [level] é de preenchimento obrigatório!")
   private String level;
 
   @ManyToOne()
   @JoinColumn(name = "company_id", insertable = false, updatable = false)
   private CompanyEntity companyEntity;
 
-  @Column(name = "company_id")
+  @Column(name = "company_id", nullable = false)
   private UUID companyId;
   
   @CreationTimestamp
