51d093c0e0d728b7eb6e7ae810023e62bc9b1c89
Juan Pina <juanpinaribeiro@outlook.com>
2024-11-05 12:29:02 -0300

    Teste: validando se o Job não existe

--- src/test/java/br/com/juanribeiro/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
+++ src/test/java/br/com/juanribeiro/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
@@ -1,5 +1,9 @@
 package br.com.juanribeiro.gestao_vagas.modules.candidate.useCases;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,9 +11,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Optional;
+import java.util.UUID;
 
+import br.com.juanribeiro.gestao_vagas.exceptions.JobNotFoundException;
 import br.com.juanribeiro.gestao_vagas.exceptions.UserNotFoundException;
+import br.com.juanribeiro.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.juanribeiro.gestao_vagas.modules.candidate.CandidateRepository;
 import br.com.juanribeiro.gestao_vagas.modules.company.repositories.JobRepository;
 
@@ -34,4 +41,22 @@ public class ApplyJobCandidateUseCaseTest {
       assertThat(e).isInstanceOf(UserNotFoundException.class);
     }
   }
+
+  @Test
+  public void should_not_be_able_to_apply_job_with_job_not_found() {
+    
+    var idCandidate = UUID.randomUUID();
+
+    var candidate = new CandidateEntity();
+    candidate.setId(idCandidate);
+
+    when(candidateRepository.findById(idCandidate)).thenReturn(Optional.of(candidate));
+    
+    try {
+      applyJobCandidateUseCase.execute(idCandidate, null);
+    } catch (Exception e) {
+      assertThat(e).isInstanceOf(JobNotFoundException.class);
+    }
+  }
+  
 }
