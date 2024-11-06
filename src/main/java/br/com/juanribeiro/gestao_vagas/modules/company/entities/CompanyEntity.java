c3bed3c5a844928287b9034d0cecb478b346b8cf
Juan Pina <juanpinaribeiro@outlook.com>
2024-11-06 15:02:50 -0300

    JobController: Inserindo banco de dados em memória para teste

--- src/main/java/br/com/juanribeiro/gestao_vagas/modules/company/entities/CompanyEntity.java
+++ src/main/java/br/com/juanribeiro/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -13,9 +13,15 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity(name = "company")
 public class CompanyEntity {
   
--- src/test/java/br/com/juanribeiro/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
+++ src/test/java/br/com/juanribeiro/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -17,19 +18,23 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import br.com.juanribeiro.gestao_vagas.modules.company.dto.CreateJobDTO;
+import br.com.juanribeiro.gestao_vagas.modules.company.entities.CompanyEntity;
+import br.com.juanribeiro.gestao_vagas.modules.company.repositories.CompanyRepository;
 import br.com.juanribeiro.gestao_vagas.utils.TestUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class CreateJobControllerTest {
 
   private MockMvc mvc;
 
   @Autowired
   private WebApplicationContext context;
+
+  @Autowired
+  private CompanyRepository companyRepository;
 
   @Before
   public void setup() {
@@ -42,6 +47,16 @@ public class CreateJobControllerTest {
   @Test
   public void should_be_able_to_create_a_new_job() throws Exception {
 
+    var company = CompanyEntity.builder()
+      .description("description_company_test")
+      .email("email@company.com")
+      .password("1234567890")
+      .username("company_username_test")
+      .name("company_name_test")
+      .build();
+
+    company = companyRepository.saveAndFlush(company);
+
     var createJobDTO = CreateJobDTO.builder()
       .benefits("BENEFITS_TEST")
       .description("DESCRIPTION_TEST")
@@ -51,7 +66,7 @@ public class CreateJobControllerTest {
     var result = mvc.perform(MockMvcRequestBuilders.post("/company/job/")
       .contentType(MediaType.APPLICATION_JSON)
       .content(TestUtils.objectToJson(createJobDTO))
-      .header("Authorization", TestUtils.generateToken(UUID.fromString("1ebcffe9-7800-4c69-9342-a5b54981f862"), "JAVAGAS_@123#"))
+      .header("Authorization", TestUtils.generateToken(company.getId(), "JAVAGAS_@123#"))
     ).andExpect(MockMvcResultMatchers.status().isOk());
 
     System.out.println(result);
