eabb2344b77e436c6dcb3e0162589a920ca3ad43
Juan Pina <juanpinaribeiro@outlook.com>
2024-11-05 22:31:56 -0300

    Criando controller Apply Job

--- src/main/java/br/com/juanribeiro/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/juanribeiro/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.juanribeiro.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.juanribeiro.gestao_vagas.modules.candidate.dto.ProfileCandidateResponseDTO;
+import br.com.juanribeiro.gestao_vagas.modules.candidate.useCases.ApplyJobCandidateUseCase;
 import br.com.juanribeiro.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
 import br.com.juanribeiro.gestao_vagas.modules.candidate.useCases.ListAllJobsByFilterUseCase;
 import br.com.juanribeiro.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
@@ -43,6 +44,9 @@ public class CandidateController {
 
   @Autowired
   private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
+
+  @Autowired
+  private ApplyJobCandidateUseCase applyJobCandidateUseCase;
 
   @PostMapping("/")
   @Operation(summary = "Cadastro de candidato", description = "Essa função é responsável por cadastrar um candidato")
@@ -93,6 +97,23 @@ public class CandidateController {
   @SecurityRequirement(name = "jwt_auth")
   public List<JobEntity> findJobByFilter(@RequestParam String filter) {
     return this.listAllJobsByFilterUseCase.execute(filter);
+  }
+
+  @PostMapping("/job/apply")
+  @PreAuthorize("hasRole('CANDIDATE')")
+  @Operation(summary = "Inscrição do candidato à vaga", description = "Essa função é responsável por inscrever um candidato a uma vaga")
+  @SecurityRequirement(name = "jwt_auth")
+  public ResponseEntity<Object> applyJob(HttpServletRequest request, @RequestBody UUID idJob) {
+
+    var idCandidate = request.getAttribute("candidate_id");
+
+    try {
+      var result = this.applyJobCandidateUseCase.execute(UUID.fromString(idCandidate.toString()), idJob);
+      return ResponseEntity.ok().body(result);
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
   }
 
 }
