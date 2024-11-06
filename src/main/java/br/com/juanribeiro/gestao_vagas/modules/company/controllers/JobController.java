45a0c337cee6b963bc30722753e0c49bb9a0d37b
Juan Pina <juanpinaribeiro@outlook.com>
2024-11-06 15:25:06 -0300

    JobController: Criando caso de teste com exceção

--- src/main/java/br/com/juanribeiro/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/juanribeiro/gestao_vagas/modules/company/controllers/JobController.java
@@ -3,6 +3,7 @@ package br.com.juanribeiro.gestao_vagas.modules.company.controllers;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,18 +43,24 @@ public class JobController {
     })
   })
   @SecurityRequirement(name = "jwt_auth")
-  public JobEntity create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
+  public ResponseEntity<Object> create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
 
     var companyId = request.getAttribute("company_id");
 
-    var jobEntity = JobEntity.builder()
+    try {
+      var jobEntity = JobEntity.builder()
       .companyId(UUID.fromString(companyId.toString()))
       .benefits(createJobDTO.getBenefits())
       .description(createJobDTO.getDescription())
       .level(createJobDTO.getLevel())
       .build();
 
-    return this.createJobUseCase.execute(jobEntity);
+      var result = this.createJobUseCase.execute(jobEntity);
+      return ResponseEntity.ok().body(result);
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(e);
+    }
+
   }
 
 }
--- src/test/java/br/com/juanribeiro/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
+++ src/test/java/br/com/juanribeiro/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
@@ -1,5 +1,7 @@
 package br.com.juanribeiro.gestao_vagas.modules.company.controllers;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.UUID;
 
 import org.junit.Before;
@@ -18,6 +20,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import br.com.juanribeiro.gestao_vagas.exceptions.CompanyNotFoundException;
 import br.com.juanribeiro.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.juanribeiro.gestao_vagas.modules.company.entities.CompanyEntity;
 import br.com.juanribeiro.gestao_vagas.modules.company.repositories.CompanyRepository;
@@ -70,6 +73,26 @@ public class CreateJobControllerTest {
     ).andExpect(MockMvcResultMatchers.status().isOk());
 
     System.out.println(result);
+  }
+
+  @Test
+  public void should_not_be_able_to_create_a_new_job_if_company_not_found() throws Exception {
+
+    var createJobDTO = CreateJobDTO.builder()
+    .benefits("BENEFITS_TEST")
+    .description("DESCRIPTION_TEST")
+    .level("LEVEL_TEST")
+    .build();
+
+    try {
+      mvc.perform(MockMvcRequestBuilders.post("/company/job/")
+      .contentType(MediaType.APPLICATION_JSON)
+      .content(TestUtils.objectToJson(createJobDTO))
+      .header("Authorization", TestUtils.generateToken(UUID.randomUUID(), "JAVAGAS_@123#")));
+    } catch (Exception e) {
+      assertThat(e instanceof CompanyNotFoundException);
+    }
+
   }
 
 }
